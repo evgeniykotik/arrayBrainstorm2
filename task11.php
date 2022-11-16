b84a96206b3b7435e2e2c2f9656ec80fe19f4d01
evgeniykotik <postoyalkoevgeniy@gmail.com>
2022-11-16 16:10:45 +0300

    task11

---
+++ task11.php
@@ -0,0 +1,25 @@
+<?php
+
+function isNullArray($arr)
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        if ($arr[$i] !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function task11($array)
+{
+    $result = [];
+    $count = count($array);
+    for ($i = 0; $i < count($array); $i++) {
+        if (!isNullArray($array[$i])) {
+            $result[] = $array[$i];
+        }
+    }
+    return $result;
+}
+
+var_dump(task11([[0, 0, 0], [0, 1, 3, 4, 5]]));
